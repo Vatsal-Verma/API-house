aaef72244d13a7634b383aae35a4e7f17156831f
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-10-17 19:02:47 +0530

    Update ApiEntity.java

--- src/main/java/com/api_house/api_house/ApiEntity.java
+++ src/main/java/com/api_house/api_house/ApiEntity.java
@@ -1,7 +1,7 @@
 package com.api_house.api_house;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
