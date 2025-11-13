3d735c72a16dd604281a272285ffa2c03892a918
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-11-13 23:47:46 +0530

    Update Controller.java
    fixed CORS problem

--- src/main/java/com/api_house/api_house/Controller.java
+++ src/main/java/com/api_house/api_house/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class Controller {
     
     @Autowired
